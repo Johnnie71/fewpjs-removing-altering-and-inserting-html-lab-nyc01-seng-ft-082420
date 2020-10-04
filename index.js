9c9feb3f6192ed1d8d44086547bda6280dad3bc5
johnnie71 <johnnie.agonz71@gmail.com>
2020-10-04 01:11:44 -0400

    Done.

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
 document.querySelector("main#main").remove();
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.innerHTML = "Johnnie is the champion";
